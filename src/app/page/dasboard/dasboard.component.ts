1a0aa7bcf6668d2dc82fc92ef4c23725e1786ff1
SebGomza@users.noreply.github.com <sebas71an-17@hotmail.com>
2024-04-27 15:54:02 -0500

    propiedad boton dashboard

--- src/app/page/dasboard/dasboard.component.ts
+++ src/app/page/dasboard/dasboard.component.ts
@@ -1,16 +1,27 @@
 import { Component } from '@angular/core';
 import { RouterLink, RouterOutlet } from '@angular/router';
 import { HeaderComponent } from '../../shared/header/header.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-dasboard',
   standalone: true,
-  imports: [RouterOutlet,RouterLink,HeaderComponent],
+  imports: [RouterOutlet,RouterLink,HeaderComponent,DasboardComponent,  CommonModule],
   templateUrl: './dasboard.component.html',
   styleUrl: './dasboard.component.scss'
 })
 export class DasboardComponent {
 
+  public prueba = 0;
 
+  siguiente () {
+    this.prueba++ ;
+  }
+  
+  bajo_riesgo: boolean = false;
+  alto_riesgo: boolean = true;
+  
 
 }
+
+
