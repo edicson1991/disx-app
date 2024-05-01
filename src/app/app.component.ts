29c89a42783df2774fb9df5fcd3fe684e67707ac
console.log('Edicson') <edicson_hurtado@iMac-de-EdicsonHurtado.local>
2024-04-30 20:36:48 -0500

    iframe

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,13 +6,12 @@ import { FooterComponent } from './shared/footer/footer.component';
 import { DasboardComponent } from './page/dasboard/dasboard.component';
 import { FormsModule } from '@angular/forms';
 import { PruebaComponent } from './prueba/prueba.component';
-import { MatButtonModule } from '@angular/material/button';
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, RouterLink, HeaderComponent, FooterComponent, BannerComponent, DasboardComponent, FormsModule, PruebaComponent, MatButtonModule],
+  imports: [RouterOutlet, RouterLink, HeaderComponent, FooterComponent, BannerComponent, DasboardComponent, FormsModule, PruebaComponent],
   templateUrl:'./app.component.html',
   styleUrl: './app.component.scss'
   
--- src/app/page/dasboard/dasboard.component.ts
+++ src/app/page/dasboard/dasboard.component.ts
@@ -2,14 +2,13 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, RouterLink, RouterOutlet } from '@angular/router';
 import { HeaderComponent } from '../../shared/header/header.component';
 import { CommonModule } from '@angular/common';
-import { MatButtonModule } from '@angular/material/button';
 import { PruebaComponent } from "../../prueba/prueba.component";
 
 
 @Component({
   selector: 'app-dasboard',
   standalone: true,
-  imports: [RouterOutlet, RouterLink, HeaderComponent, DasboardComponent, CommonModule, MatButtonModule, PruebaComponent],
+  imports: [RouterOutlet, RouterLink, HeaderComponent, DasboardComponent, CommonModule,  PruebaComponent], 
   templateUrl: './dasboard.component.html',
   styleUrl: './dasboard.component.scss'
    
